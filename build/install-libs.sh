9766b54969ce9e8edea0e49f77ce28c31123eb44
Muhammad Rafat Kurkar <84540534+KMKINGMAN@users.noreply.github.com>
2024-06-24 02:23:28 +0300

    add one more lib to install

--- build/install-libs.sh
+++ build/install-libs.sh
@@ -6,15 +6,13 @@ if [ "$(command -v apt)" ]; then
   echo "Updating apt package manager..."
   sudo apt update
   echo "Installing required packages..."
-  sudo apt install libglib2.0-dev
-  sudo apt install libpoppler-glib8
-  sudo apt install libpoppler-glib-dev
+  sudo apt install -y libglib2.0-dev libpoppler-glib-dev pkg-config
 elif [ "$(command -v dnf)" ]; then
   # Update package manager and install for Fedora/CentOS
   echo "Updating dnf package manager..."
   sudo dnf update
   echo "Installing required packages..."
-  sudo dnf install glib2 poppler-glib-devel
+  sudo dnf install -y glib2-devel poppler-glib-devel pkgconf-pkg-config
 else
   echo "Error: Unsupported operating system."
   exit 1
